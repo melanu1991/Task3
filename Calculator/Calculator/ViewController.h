78a729846ea09e66cf0d0bb6da504e4fd65e3b1f
melanu1991 <melanu284733@gmail.com>
2017-05-14 13:12:20 +0300

    Create project

---
+++ Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by melanu on 14.05.17.
+//  Copyright © 2017 melanu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by melanu on 14.05.17.
+//  Copyright © 2017 melanu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
