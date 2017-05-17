0d8f7157b2088bc6d36e3313f2b3940d012ad2da
melanu1991 <melanu284733@gmail.com>
2017-05-17 17:56:14 +0300

    create class CalculatorModel

---
+++ Calculator/Calculator/CalculatorModel.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorModel.h
+//  Calculator
+//
+//  Created by melanu1991 on 17.05.17.
+//  Copyright © 2017 melanu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorModel : NSObject
+
+@end
---
+++ Calculator/Calculator/CalculatorModel.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorModel.m
+//  Calculator
+//
+//  Created by melanu1991 on 17.05.17.
+//  Copyright © 2017 melanu. All rights reserved.
+//
+
+#import "CalculatorModel.h"
+
+@implementation CalculatorModel
+
+@end
